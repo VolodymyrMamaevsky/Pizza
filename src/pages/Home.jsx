d8c7d901481c3783457a1e4304a99add1817ae5f
Volodymyr Mamaevsky <vova.moder@gmail.com>
2023-06-22 15:14:52 +0300

    add Axios in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useContext } from "react";
+import React from "react";
+import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 
@@ -15,42 +16,41 @@ function Home() {
 
   const dispatch = useDispatch();
 
-  const { searchValue } = useContext(SearchContext);
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
+  const { searchValue } = React.useContext(SearchContext);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [currentPage, setCurrentPage] = React.useState(1);
 
-  const onClickCategory = (id) => {
+  const onCLickCategory = (id) => {
     dispatch(setCategoryId(id));
   };
 
-  const category = categoryId > 0 ? `category=${categoryId}` : ``;
-  const sortBy = sortProp.sort.replace("-", "");
-  const order = sortProp.sort[0] === `-` ? `desc` : `asc`;
-  const search = searchValue ? `&search=${searchValue}` : ``;
-
-  useEffect(() => {
+  React.useEffect(() => {
     setIsLoading(true);
-    fetch(
-      `https://6454c0bbf803f34576304938.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
+
+    const category = categoryId > 0 ? `category=${categoryId}` : ``;
+    const sortBy = sortProp.sort.replace("-", "");
+    const order = sortProp.sort[0] === `-` ? `desc` : `asc`;
+    const search = searchValue ? `&search=${searchValue}` : ``;
+
+    axios
+      .get(
+        `https://6454c0bbf803f34576304938.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+      )
+      .then((res) => {
+        setItems(res.data);
         setIsLoading(false);
       });
   }, [categoryId, sortProp.sort, searchValue, currentPage]);
 
   const pizzas = items.map((pizza) => <PizzaBlock {...pizza} key={uuidv4()} />);
-
   const skeletons = [...new Array(4)].map((_, index) => (
     <Skeleton key={uuidv4()} />
   ));
-
   return (
     <div className="container">
       <div className="content__top">
-        <Categories categoryId={categoryId} onClickCategory={onClickCategory} />
+        <Categories categoryId={categoryId} onClickCategory={onCLickCategory} />
         <Sort />
       </div>
       <h2 className="content__title">All pizzas</h2>
@@ -59,5 +59,4 @@ function Home() {
     </div>
   );
 }
-
 export default Home;
